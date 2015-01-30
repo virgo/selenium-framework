00d1070e279f27f11b6460830b9e50ba89b450de
Horváth Péter Attila <horvath.peter@virgo.hu>
2015-01-30 16:09:09 +0100

    Exception

---
+++ src/main/java/hu/virgo/selenium/framework/page/verify/PageVerificationException.java
@@ -0,0 +1,14 @@
+package hu.virgo.selenium.framework.page.verify;
+
+public class PageVerificationException extends RuntimeException {
+
+	private static final long serialVersionUID = 6522856354923357348L;
+
+	public PageVerificationException() {
+		super();
+	}
+
+	public PageVerificationException(String message) {
+		super(message);
+	}
+}
