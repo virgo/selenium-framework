98cc4a49a40f7936ec67922eb63c6406ae500ad1
Horváth Péter Attila <horvath.peter@virgo.hu>
2015-02-04 10:56:42 +0100

    Debug Data Store

---
+++ src/main/java/hu/virgo/selenium/debug/DebugDataStore.java
@@ -0,0 +1,21 @@
+package hu.virgo.selenium.debug;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import com.google.gson.Gson;
+
+public class DebugDataStore {
+
+	private Map<String, Object> store = new HashMap<String, Object>();
+
+	public void put(String key, Object value) {
+		store.put(key, value);
+	}
+
+	public String flush() {
+		Gson gson = new Gson();
+		return gson.toJson(store);
+	}
+
+}
--- src/main/java/hu/virgo/selenium/framework/JUnitTestTemplate.java
+++ src/main/java/hu/virgo/selenium/framework/JUnitTestTemplate.java
@@ -1,32 +1,43 @@
 package hu.virgo.selenium.framework;
 
+import hu.virgo.selenium.debug.DebugDataStore;
 import hu.virgo.selenium.framework.context.Context;
 
-import org.junit.After;
-import org.junit.Before;
-import org.junit.BeforeClass;
+import org.junit.Rule;
+import org.junit.rules.TestWatcher;
+import org.junit.runner.Description;
 import org.openqa.selenium.WebDriver;
 
 public class JUnitTestTemplate {
 
-	protected static Context context;
+	protected static Context context = new Context();
 	protected WebDriver driver;
 	protected Utils utils;
+	protected DebugDataStore debugData;
 
-	@BeforeClass
-	public static void initContext() {
-		context = new Context();
-	}
+	@Rule
+	public TestWatcher watchman = new TestWatcher() {
 
-	@Before
-	public void initTest() {
-		driver = context.getNewDriver();
-		utils = new Utils(driver);
-	}
+		@Override
+		protected void starting(Description description) {
+			driver = context.getNewDriver();
+			debugData = new DebugDataStore();
+			utils = new Utils(driver);
+		}
 
-	@After
-	public void tearDown() {
-		driver.quit();
-		driver = null;
-	}
+		@Override
+		protected void failed(Throwable e, Description description) {
+			debugData.put("testCase", description.getClassName() + "." + description.getMethodName());
+			debugData.put("cookies", driver.manage().getCookies());
+			debugData.put("currentUrl", driver.getCurrentUrl());
+			System.err.println(debugData.flush());
+		}
+
+		@Override
+		protected void finished(Description description) {
+			driver.quit();
+			driver = null;
+		}
+	};
+
 }
